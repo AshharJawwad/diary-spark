6f4f2dfc9606e5f1b70ad5bc16326e170ef900dd
Ashhar Jawwad <rockashhar10@gmail.com>
2026-05-02 13:19:17 +0530

    Fully responsive and optimized the Navbar

--- app/page.js
+++ app/page.js
@@ -1,15 +1,8 @@
 export default function Home() {
   return (
-    <div className="m-auto w-6xl h-full p-5 justify-center items-center">
-      {/* Feature Screeen */}
-      <div className="min-h-96 w-5xl m-auto bg-red-500 rounded-lg relative">
-        <div className="h-10 w-36 bg-amber-300 absolute flex rounded-br-lg rounded-tl-lg bottom-0 right-0 justify-center items-center space-x-5">
-          <div className="h-2 w-2 cursor-pointer hover:h-2.5 hover:w-2.5 rounded-full bg-white"></div>
-          <div className="h-2 w-2 cursor-pointer hover:h-2.5 hover:w-2.5 rounded-full bg-white"></div>
-          <div className="h-2 w-2 cursor-pointer hover:h-2.5 hover:w-2.5 rounded-full bg-white"></div>
-          <div className="h-2 w-2 cursor-pointer hover:h-2.5 hover:w-2.5 rounded-full bg-white"></div>
-        </div>
-      </div>
+    <div className="m-auto min-h-full p-5 justify-center items-center bg-purple-500">
+      {/* Hero Section */}
+      
 
       {/* Content Section */}
       <div className=""></div>
--- components/Navbar.js
+++ components/Navbar.js
@@ -11,10 +11,12 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet";
 import { ModeToggle } from "./ui/theme-btn";
+import { LogIn } from "lucide-react";
+import { Cog } from "lucide-react";
 
 const Navbar = () => {
   return (
-    <nav className="w-full flex items-center sticky justify-around px-6 py-4 top-0 border-b bg-background/80 backdrop-blur-md">
+    <nav className="w-full flex items-center sticky justify-between md:justify-around px-6 py-4 top-0 border-b bg-background/80 backdrop-blur-md">
       {/* Logo */}
       <Link href={"/"}>
         <div className="text-2xl font-bold text-gray-900 dark:text-white tracking-tight">
@@ -24,30 +26,39 @@ const Navbar = () => {
 
       {/* Navigation Menu*/}
       <div className="hidden md:flex space-x-8 items-center text-gray-700 dark:text-white">
-        <Link href="/" className="text-lg font-semibold">
+        <Link href="/" className="text-lg font-semibold hover:text-violet-500">
           Home
         </Link>
-        <Link href="/blog" className="text-lg font-semibold">
+        <Link
+          href="/blog"
+          className="text-lg font-semibold hover:text-violet-500"
+        >
           Blog
         </Link>
-        <Link href="/story" className="text-lg font-semibold">
+        <Link
+          href="/story"
+          className="text-lg font-semibold hover:text-violet-500"
+        >
           Story
         </Link>
-        <Link href="/quiz" className="text-lg font-semibold">
+        <Link
+          href="/quiz"
+          className="text-lg font-semibold hover:text-violet-500"
+        >
           Quiz
         </Link>
       </div>
 
       {/* Right Side Buttons */}
       <div className="flex items-center gap-4">
-        <Button className="hidden md:block px-4 py-2 text-center rounded-md border-b text-white hover:bg-violet-600 transition">
-          Register/SignIn
+        <Button className="hidden cursor-pointer h-10 w-11 md:block px-4 py-2 text-center items-center rounded-sm border-b text-white hover:bg-violet-500 transition">
+          <LogIn size={20} className="justify-center items-center" />
         </Button>
 
         <div className="hidden"></div>
         <ModeToggle />
 
-        <div className="block md:hidden items-center">
+        <div className="md:hidden items-center">
           {/* Tab and Mobile Navigatiopn Menu */}
           <Sheet>
             <SheetTrigger asChild>
@@ -55,35 +66,48 @@ const Navbar = () => {
                 <Menu className="h-6 w-6" />
               </button>
             </SheetTrigger>
-            <SheetContent>
+            <SheetContent className={"w-full overflow-hidden"}>
               <SheetHeader>
                 <SheetTitle className={"text-2xl font-bold my-3 p-4"}>
                   DiarySpark
                 </SheetTitle>
                 <SheetDescription>
-                  <div className="flex flex-col gap-8 w-full text-center my-4">
-                    <Link href="/" className="text-sm font-semibold">
+                  <div className="flex flex-col gap-4 w-full text-center my-1">
+                    <Link href="/" className="text-lg font-semibold">
                       Home
                     </Link>
-                    <Link href="/blog" className="text-sm font-semibold">
+                    <Link href="/blog" className="text-lg font-semibold">
                       Blog
                     </Link>
-                    <Link href="/story" className="text-sm font-semibold">
+                    <Link href="/story" className="text-lg font-semibold">
                       Story
                     </Link>
-                    <Link href="/quiz" className="text-sm font-semibold">
+                    <Link href="/quiz" className="text-lg font-semibold">
                       Quiz
                     </Link>
                     <div className="h-18 w-full my-10 p-4 justify-center items-center">
                       <div className="text-start text-lg font-bold">
                         Accessibility
                       </div>
+                      <div className="flex w-full items-center justify-center mt-6 gap-3">
+                        <Button
+                          className={"items-center w-1/3 px-4 py-2"}
+                        ></Button>
+                        <Button
+                          className={
+                            "items-center w-1/3 px-4 py-2 bg-white border-2 border-violet-400 text-gray-900 dark:text-white dark:bg-black"
+                          }
+                        ></Button>
+                        <Button className={"items-center w-1/3 px-4 py-2"}>
+                          <Cog />
+                        </Button>
+                      </div>
                     </div>
-                    <div className="flex mt-12 w-full h-8 p-4 justify-center items-center">
-                      <Button className="mx-1 w-1/2 text-xs px-4 py-2 rounded-md text-gray-700 bg-white hover:bg-gray-100 transition">
+                    <div className="flex mt-8 w-full h-8 p-4 justify-center items-center">
+                      <Button className="mx-1 w-1/2 cursor-pointer text-sm px-4 py-2 rounded-md text-gray-700 bg-white border-2 border-violet-400 dark:text-white dark:bg-black hover:bg-gray-300 transition">
                         Register
                       </Button>
-                      <Button className="mx-1 w-1/2 text-xs px-4 py-2 rounded-md hover:bg-violet-600 transition">
+                      <Button className="mx-1 w-1/2 text-sm px-4 py-2 rounded-md hover:bg-violet-600 transition">
                         SignIn
                       </Button>
                     </div>
