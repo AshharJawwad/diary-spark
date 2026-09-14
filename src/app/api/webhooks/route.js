b6d88853b34f6aed7a4b29c542e2d9b20c8fb9ac
Ashhar Jawwad <rockashhar10@gmail.com>
2026-09-14 19:46:00 +0530

    fix

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -1,5 +1,4 @@
 import { verifyWebhook } from '@clerk/nextjs/webhooks'
-
 export async function POST(req) {
   try {
     const evt = await verifyWebhook(req)
@@ -10,7 +9,6 @@ export async function POST(req) {
     const eventType = evt.type
     console.log(`Received webhook with ID ${id} and event type of ${eventType}`)
     console.log('Webhook payload:', evt.data)
-    
 
     return new Response('Webhook received', { status: 200 })
   } catch (err) {
